22ede1b5d0f8d0434b4e7db51c2f3f4fd0619cc4
Erik C Nelson <ecnelson@data.cs.purdue.edu>
2021-09-17 17:01:01 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,40 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm Memes/SpongeBob/money.jpg
 
 # Task 3
-
+mv Memes/'Star Wars'/fafsa.jpg Memes/SpongeBob/fafsa.jpg
 
 # Task 4
-
+rmdir Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++/Age.cpp
 
 # Task 7
-
+cp-r Programs/Java Homework/CS180/Project_1
 
 # Task 8
-
+mv Programs/Count.c Programs/C/Count.c && Programs/Goodbye.c Programs/C/Goodbye.c
 
 # Task 9
-
+mv Programs/Name.py Programs/'Python 3'/Name.py && Programs/Space.py Programs/'Python 3'/Space.py
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls /homes/ecnelson
 
 # Task 12
-: '
-Type your vim/nano customizations below:
-
-
-
-'
+:Type your vim/nano customizations below:
+echo >> "set mouse=a" ~/.vimrc
+echo >> "syntax on" ~/.vimrc
+echo >> "set autoread" ~/.vimrc
